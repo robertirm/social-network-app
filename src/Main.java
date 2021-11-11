f7cdb2f08512237ab4664efcda39e85b510fc4c7
Hodosi <hodosi.attila@yahoo.com>
2021-11-11 23:16:08 +0200

    Finish practice task

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hey");
         System.out.println("Hey - Eu nu mai sunt conflict");
+        System.out.println("Final Code");
     }
 }
