c5e2d10926dd696bf1aee40f04bc56def7d6e7e2
Hodosi <hodosi.attila@yahoo.com>
2021-11-14 12:40:42 +0200

    Added test folder

---
+++ src/test/java/socialNetwork/domain/UserTest.java
@@ -0,0 +1,24 @@
+package socialNetwork.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void getFriendList() {
+    }
+}
